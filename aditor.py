708f4b9a111bfbc7757ddd05f5bcad20e2834a51
Natan <natanknioch@gmail.com>
2026-05-07 10:45:20 +0000

    adding gitignore to the branch and adding more functionality

--- aditor.py
+++ aditor.py
@@ -23,10 +23,37 @@ def audit_pod_security(namespace="free5gc"):
                 print(f"{container.name} ~ privileged")
             if securityContext.run_as_user==0 and securityContext.run_as_none_root is False:
                 print(f"{container.name} ~ run as root")
-            if securityContext.allow_privileg_escalation:
+            if securityContext.allow_privilege_escalation:
                 print(f"{container.name} ~ allow privileged escaletion")
+
+            #flie system access
+            if securityContext.read_only_root_filesystem is False:
+                print(f"{container.name} ~ read only root file system is false")
+            if securityContext.seccomp_profile == None:
+                print(f"{container.name} ~ seccomp is not set")
+            
+            #recource access
+            risky_mounts = ["/", "/etc", "/root", "/var/run/docker.sock", "/var/log"]
+
+            if container.resources.limits == None:
+                print(f"{container.name} ~ no resource limits")
+            else:
+                if container.resources.limits.get("cpu") == None:
+                    print(f"{container.name} ~ no cpu limit")
+                if container.resources.limits.get("memory") == None:
+                    print(f"{container.name} ~ no memory limit")
+
+            for volume in pod.spec.volumes:
+                if volume.host_path and volume.host_path.path in risky_mounts:
+                    print(f"{container.name} ~ risky host path mount: {volume.host_path.path}")
+            
+            #capabilities
             caps = securityContext.capabilities
-            print(caps)
+            if caps is None:
+                print(f"{container.name} ~ capabilities are not set")
+            else:
+                if caps.drop is None or "ALL" not in caps.drop:
+                    print(f"{container.name} ~ does not drop all capabilities")
 
 if __name__ == "__main__":
     audit_pod_security()
